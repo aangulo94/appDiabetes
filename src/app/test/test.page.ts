3ea9228ab15d80a2680e69dd83f674a252a9e84b
Diego Carvajal <diegofcarvajalf@gmail.com>
2019-05-13 14:42:59 +0200

    funcionalidad del test

--- src/app/test/test.page.ts
+++ src/app/test/test.page.ts
@@ -10,16 +10,18 @@ export class TestPage{
   
   fem: boolean = false;
   mal: boolean = false;
-  riskcounter:any;
+  riskcounter:any =0;
   questionCounter: any = 1;
-  value: any;
+  value: any=0;
+  value_extra: any=0;
   
   constructor(private router:Router){}
   
   nextQuestion(){
-    this.riskcounter =+ this.value;
-    this.value = 0;
-    if (this.questionCounter == 9) 
+    this.riskcounter = this.riskcounter+this.value;
+    this.value=0;
+    this.value_extra=0;
+     if (this.questionCounter == 10) 
       this.questionCounter = this.questionCounter;
       
     else
@@ -30,15 +32,23 @@ export class TestPage{
   //QUESTION 1
   female(){
     this.fem= true;
-    this.riskcounter= 0;
+    this.mal=false;
+    
+    
+    
   }
   male(){
     this.mal= true;
-    this.riskcounter= 0;
+    this.fem=false;
+    
   }
   
   //QUESTION 2
   
+  age1(){
+    this.value_extra =2;
+    this.value =0;
+  }
   age2(){
     this.value =2;
   }
@@ -50,6 +60,10 @@ export class TestPage{
   }
   
   //QUESTION 3
+  bmi1(){
+    this.value_extra = 2;
+    this.value =0;
+  }
   
   bmi2(){
     this.value = 1;
@@ -60,6 +74,10 @@ export class TestPage{
   
   //QUESTION 4
   
+  waist1(){
+    this.value_extra = 2;
+    this.value =0;
+  }
   waist2(){
     this.value = 3;
   }
@@ -67,16 +85,28 @@ export class TestPage{
     this.value = 4;
   }
   //QUESTION 5
+  bloodpressure_extra(){
+    this.value_extra = 2;
+    this.value =0;
+    }
   bloodpressure(){
     this.value = 2;
   }
   
   //QUESTION 6
+  bloodglucose_extra(){
+    this.value_extra = 2;
+    this.value =0;
+    }
   bloodglucose(){
     this.value = 5;
   }
   
   //QUESTION 7
+  activitylow_extra(){
+    this.value_extra = 2;
+    this.value =0;
+  }
   activitylow(){
     this.value = 2;
   }
@@ -85,9 +115,17 @@ export class TestPage{
   vegetables(){
     this.value = 1;
   }
+  vegetables_extra(){
+    this.value_extra = 2;
+    this.value =0;
+  }
   
   //QUESTION 9
-  
+
+  familyhistory1(){
+    this.value_extra = 2;
+    this.value =0;
+  }
   familyhistory2(){
     this.value = 3;
   }
